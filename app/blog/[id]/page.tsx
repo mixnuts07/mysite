b7050f05ad91bd628d7a3270e533d76c01804405
mixnuts07 <french0719@gmail.com>
2023-12-03 15:22:54 +0900

    fix page

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,26 +1,39 @@
 // "use client";
 // import { client, fetchEntry } from "@/contentful";
 // import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-// import { useState } from "react";
+// import { use, useEffect, useState } from "react";
+
+// async function get(id: string) {
+//   const entry = await client.getEntry(id);
+//   const title = entry.fields.title ? entry.fields.title.toString() : "no title";
+//   const body = entry.fields.body;
+//   console.log(title);
+//   console.log(documentToReactComponents(entry.fields.body));
+// }
 
 // // eslint-disable-next-line @next/next/no-async-client-component
 // export default async function Page({ params }: { params: { id: string } }) {
-//   const [title, setTitle] = useState<string | undefined>(undefined);
 //   // const entry = await client.getEntry(params.id);
 //   // const title = entry.fields.title ? entry.fields.title.toString() : "no title";
 //   // const body = entry.fields.body;
-//   const entry = await fetchEntry(params.id);
-//   console.log(entry);
-//   setTitle(entry?.fields.title?.toString());
-//   // const firstItem = entry?.items[0];
 //   return (
 //     <div className="mt-10">
-//       <h1>{title}</h1>
+//       {/* <h1>{title}</h1> */}
 //       {/* <p>{documentToReactComponents(body)}</p> */}
 //       <h1>te</h1>
 //       <h1>te</h1>
 //       <h1>te</h1>
+//       <button onClick={() => get(params.id)}>click</button>
 //     </div>
 //   );
 // }
 // //
+
+export default function Page({ params }: { params: { id: string } }) {
+  return (
+    <div className="mt-10">
+      <h1>todo</h1>
+      <h1>id is {params.id}</h1>
+    </div>
+  );
+}
