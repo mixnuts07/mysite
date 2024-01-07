2818102c4000de139130dae6c05b0482a6669a42
mixnuts07 <french0719@gmail.com>
2024-01-07 21:57:38 +0900

    add vercel analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <main className="flex flex-col items-center min-w-full min-h-screen bg-white text-black font-mono">
           <Header />
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
