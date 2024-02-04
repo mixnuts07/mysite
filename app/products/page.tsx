d95c213e604b5ceb433c6a4722be7959e45973eb
mixnuts07 <french0719@gmail.com>
2024-02-04 12:15:07 +0900

    add animation products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Products() {
-  return <div className='my-20'>coming soon...</div>;
+  return <div className='my-20 animate-ping'>coming soon...</div>;
 }
