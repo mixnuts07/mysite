089bbe4545814ffbfb20e3224d85f5f6c175fa59
mixnuts07 <french0719@gmail.com>
2024-02-05 22:52:00 +0900

    fix style

--- app/_components/ArticleComponent.tsx
+++ app/_components/ArticleComponent.tsx
@@ -9,26 +9,26 @@ import {TagsComponent} from "@/app/_components/TagsComponent";
 
 export default function ArticleComponent({props}: ArticleTypeProps) {
     return (
-        <article className='my-10 flex justify-center items-center flex-col mx-auto px-10'>
-            <article className="mt-10 space-y-3 flex flex-col items-center mx-auto w-4/5 px-20">
+        <article className='mt-10 mb-5 flex justify-center items-center flex-col w-4/5 mx-auto'>
+            <article className="space-y-3 flex flex-col justify-center items-center min-w-full px-5">
                 <TitleComponent title={props.title} className='text-xl'/>
                 <UpdatedAtComponent date={props.updatedAt}/>
                 <TagsComponent tags={props.tags}/>
             </article>
-            <article className='mt-10 break-words space-y-5 w-4/5 px-20'>
-                <Markdown remarkPlugins={[remarkGfm]} className='prose prose-stone prose-a:underline prose-a:underline-offset-4' components={{
-                    code({ node,  className, children, ...props}) {
-                    const inline = node?.tagName === "code" && !className?.includes('language-')
-                        const match = /language-(\w+)/.exec(className || '');
-                        return !inline ? (
-                            <CodeBlock language={match ? match[1] : undefined} value={String(children).replace(/\n$/, '')} {...props} />
-                        ) : (
-                            <code className={className} {...props}>
-                                {children}
-                            </code>
-                        );
-                    }}}>{props.body}</Markdown>
-            </article>
+            <Markdown remarkPlugins={[remarkGfm]} className='prose prose-a:underline prose-a:underline-offset-4 mt-10 break-words space-y-5 w-4/5 px-10' components={{
+                code({ node,  className, children, ...props}) {
+                const inline = node?.tagName === "code" && !className?.includes('language-')
+                    const match = /language-(\w+)/.exec(className || '');
+                    return !inline ? (
+                        <CodeBlock language={match ? match[1] : undefined} value={String(children).replace(/\n$/, '')} {...props} />
+                    ) : (
+                        <code className={className} {...props}>
+                            {children}
+                        </code>
+                    );
+            }}}>
+                {props.body}
+            </Markdown>
         </article>
     )
 }
