961e8ff87e469268f95978c4f7432821bcc496f1
mixnuts07 <french0719@gmail.com>
2024-05-06 23:53:39 +0900

    wip products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Products() {
   return (
       <div className='animate-ping flex items-center justify-center m-auto h-screen w-screen'>
-        coming soon...
+        {/*coming soon...*/}
       </div>
   )
 }
